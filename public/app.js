b3d7d9b3a1f8fd3e2ea72becb38175cdbaeccc12
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-19 21:02:30 +0000

    Added existing cat details to cat array and removed them from the HTML file

--- public/app.js
+++ public/app.js
@@ -1,16 +1,25 @@
-const addCat = function(name, favFood, imageHTML){
+const CAT_ARRAY = [
+
+    {name: 'Boba', favFood: 'Sock fluff', imageLink: '<img width="500" src="http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg">'},
+    {name: 'Barnaby', favFood: 'Tuna', imageLink: '<img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg">'},
+    {name: 'Max', favFood: 'Whiskas Temptations', imageLink: '<img width="500" src="http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg">'},
+    {name: 'Grumpy', favFood: 'Mice', imageLink: '<img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg">'}
+];
+
+
+const addCat = function(name, favFood, imageLink){
 
     const unorderedList = document.createElement('ul');
     unorderedList.classList.add('cat');
 
     const nameListItem = document.createElement('li');
-    nameListItem.innerText = "Name: " + name;
+    nameListItem.innerText = 'Name: ' + name;
 
     const favFoodListItem = document.createElement('li');
-    favFoodListItem.innerText = "Favourite Food: " + favFood;
+    favFoodListItem.innerText = 'Favourite Food: ' + favFood;
 
     const imageListItem = document.createElement('li');
-    imageListItem.innerHTML= imageHTML;
+    imageListItem.innerHTML= imageLink;
 
     unorderedList.appendChild(nameListItem);
     unorderedList.appendChild(favFoodListItem);
@@ -21,10 +30,11 @@ const addCat = function(name, favFood, imageHTML){
 
 }
 
-
 const app = function() {
 
-    addCat('Grumpy', 'Mice', '<img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg">')
+    for(i=0; i < CAT_ARRAY.length; i++){
+        addCat(CAT_ARRAY[i].name, CAT_ARRAY[i].favFood, CAT_ARRAY[i].imageLink);
+    }
     
 }
 
